3a77c248099fe66188c0fcc4369b52c9c600d48b
Loïc Goyet <loic@appventus.com>
2016-12-01 14:33:57 +0100

    improve javascript files generated

--- generators/app/templates/javascript/component.js
+++ generators/app/templates/javascript/component.js
@@ -1,1 +1,14 @@
-console.log('hello <%= props.names.lowercase.singular %> !');
+class Trowel<%= props.names.pascalcase.plural %> {
+    constructor(elements) {
+        elements.forEach(function(element, index) {
+            let element_obj = new Trowel<%= props.names.pascalcase.singular %>(element);
+        })
+    }
+}
+
+class Trowel<%= props.names.pascalcase.singular %> {
+    constructor(element) {
+        this.element = element;
+        console.log(this.element);
+    }
+}
